85e6cdd168274776c336325be1d8e607221da3c3
Ravi VAishnav <99575362+RAVI19099@users.noreply.github.com>
2023-12-05 07:17:36 +0000

    container config done

---
+++ .devcontainer/01_variable/vari.js
@@ -0,0 +1,5 @@
+const accounId = "12466"
+var accountEmail = "ravivai1909@"
+let accountPassword = "22222"
+accountCity = "Udaipur"
+console.table([accounId,accountEmail,accountPassword,accountCity]);
