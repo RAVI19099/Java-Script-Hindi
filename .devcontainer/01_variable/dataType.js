553e7d51c2f04d3ca9cbf84708b2e4338d8e127d
Ravi VAishnav <99575362+RAVI19099@users.noreply.github.com>
2023-12-05 09:15:08 +0000

    dataTypes

---
+++ .devcontainer/01_variable/dataType.js
@@ -0,0 +1,20 @@
+"use strict"//treat all JS code as newer version
+// alert()// we are using node js, not browser
+console.log(3+3);
+  
+let name = "Ravi"
+let age = 23
+let isLoggedIn = false
+console.table([name,age,isLoggedIn]);
+// number => 2 to power 53
+// bigint for biger integer
+// string=> ""
+// bollean=> true/false
+// null=>stadnAloneValue
+// undefine=>value not assign to memory 
+// symbols=>unique
+// object
+
+console.log(typeof age);//=>shows what datatypes of value
+ console.log(typeof null);//=> type of null is object
+console.log(typeof undefined);
