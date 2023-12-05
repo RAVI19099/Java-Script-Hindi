d6bc8a6fd366265c7960776545e5e29a44539b39
Ravi VAishnav <99575362+RAVI19099@users.noreply.github.com>
2023-12-05 13:40:08 +0000

    conversion datatype

---
+++ .devcontainer/01_variable/conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"
+// two way to type of data
+console.log(typeof score);
+console.log(typeof(score));
+let valuInNumber = Number(score)
+console.log(typeof valuInNumber);
+console.log(valuInNumber);
+// "33"=>33
+
+//"33abc"=>Nan
+// true=>1;false=>0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//1=>true; 0=>false
+// ""=>false
+// "Ravi"=true
+
+
+//* let someNumber = 33;
+// let stringNumber  = string(someNumber);
+//console.log(stringNumber);*//
